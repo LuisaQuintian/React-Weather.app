966e6215f48fabb23153fb62f4bd7b131641747a
Luisa Quintian <luisaquintian@hotmail.com>
2021-10-14 11:13:16 -0300

    Made the error message responsive without the button

--- src/components/ErrorMessage/styles.js
+++ src/components/ErrorMessage/styles.js
@@ -23,14 +23,19 @@ export const Container = styled.div`
         padding: 3rem;
         border-radius: 1.5rem;
 
+        @media (max-width: 720px){
+            height: 85%;
+        }
+
+
         h1 {
             margin-bottom: 35px;
-            font-size: 3rem;
+            font-size: 2.5rem;
             color: #EB5777;
         }
 
         p {
-            font-size: 1.5rem;
+            font-size: 1.4rem;
             margin: 5px;
             text-align: center;
         }
@@ -38,7 +43,7 @@ export const Container = styled.div`
         hr {
             width: 70%;
             height: 3px;
-            margin: 20px;
+            margin: 15px;
             text-align: center;
             
             background-color: #EB5777;
